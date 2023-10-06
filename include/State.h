d2e03997dd1e199073dc56c1c47b93fa30abae06
Çağrı Dükünlü <cagridknl@gmail.com>
2023-10-06 13:29:53 +0300

    Unused files removed

--- include/State.h
+++
@@ -1,8 +0,0 @@
-class State {
-public:
-    virtual ~State() {}
-    virtual void init() = 0;
-    virtual void update() = 0;
-    virtual void render() = 0;
-    virtual void handleInput() = 0;
-};
